3da89a0e925b505cc235d476f6ca838c50a14939
Peter Armstrong <peter@ruboss.com>
2020-05-31 00:39:37 -0700

    stuff

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-rm ~/repos/markua.org/public/spec/lfm.html
+rm spec.html
+make spec.html
+open spec.html
+rm -f ~/repos/markua.org/public/spec/lfm.html
 cp spec.html ~/repos/markua.org/public/spec/lfm.html
 
